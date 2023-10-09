71b98568dd2c7e9d916cae8417c2794b6f482ba0
JakobKaae <jakob.kaae1@gmail.com>
2023-10-09 11:01:57 +0200

    Placeholders

--- App.tsx
+++ App.tsx
@@ -8,6 +8,7 @@
 import React from 'react';
 import type { PropsWithChildren } from 'react';
 import {
+  Button,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -71,25 +72,15 @@ function App(): JSX.Element {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+          <Text>Welcome to CrossTalk</Text>
+          <Text>Please sign in</Text>
+          <Button title='Google'></Button>
+          <Button title='Facebook'></Button>
+          <Text>or create new account</Text>
         </View>
       </ScrollView>
     </SafeAreaView>
